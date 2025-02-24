5dcddcc1d04acad3de35cd54140537f76e76090e
ThienTran98 <tranhuuthien98@gmail.com>
2025-02-24 16:59:44 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,351 @@
+const data = {
+  outstandingProduct: [
+    {
+      id: 1,
+      name: "iPhone 16e 128GB | Chính hãng VN/A",
+      priceOld: "22.990.000",
+      price: "16.990.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_ng_n_11__7_1.png",
+    },
+    {
+      id: 2,
+      name: "Samsung Galaxy S25 Ultra 12GB 256GB",
+      priceOld: "33.990.000",
+      price: "30.990.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/d/i/dien-thoai-samsung-galaxy-s25-ultra_2__3.png",
+    },
+    {
+      id: 3,
+      name: "Xiaomi Redmi Note 14 6GB 128GB",
+      priceOld: "4.990.000",
+      price: "4.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/d/i/dien-thoai-xiaomi-redmi-note-14_2__2.png",
+    },
+
+    {
+      id: 4,
+      name: "Apple MacBook Air M1 256GB 2020 I Chính hãng Apple Việt Nam",
+      priceOld: "17.990.000",
+      price: "22.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/m/a/macbook_5_1.png",
+    },
+
+    {
+      id: 5,
+      name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
+      priceOld: "14.990.000",
+      price: "13.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_ng_n_19__3_19.png",
+    },
+    {
+      id: 6,
+      name: "Màn hình E-Dra EGM27F100H 27 inch 144Hz",
+      priceOld: "2.990.000",
+      price: "1.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/f/r/frame_195_25__1.jpg",
+    },
+    {
+      id: 7,
+      name: "Xiaomi Redmi Pad SE Wifi (6GB 128GB)",
+      priceOld: "6.990.000",
+      price: "5.590.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/x/i/xiaomi-redmi-pad-se_1_3.png",
+    },
+    {
+      id: 8,
+      name: "iPad Air 6 M2 11 inch Wifi 128GB | Chính hãng Apple Việt Nam",
+      priceOld: "16.990.000",
+      price: "16.190.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/i/p/ipad-air-6-m2-11-inch_9_.jpg",
+    },
+  ],
+
+  //   Máy tính
+  laptop: [
+    {
+      id: 9,
+      name: "Laptop Acer Aspire 3 Spin A3SP14-31PT-387Z",
+      priceOld: "15.990.000",
+      price: "11.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_d_i_1__3_13.png",
+    },
+    {
+      id: 10,
+      name: "Laptop MSI Modern 14 C12MO-660VN",
+      priceOld: "14.990.000",
+      price: "12.990.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_ng_n_1__2_26.png",
+    },
+    {
+      id: 11,
+      name: "Laptop ASUS Gaming VivoBook K3605ZC-RP564W",
+      priceOld: "25.990.000",
+      price: "19.490.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/g/r/group_509_-_2024-07-31t162730.053.png",
+    },
+    {
+      id: 12,
+      name: "Laptop Gaming Acer Nitro V ANV15-51-58AN",
+      priceOld: "19.990.000",
+      price: "16.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_d_i_2__2_25.png",
+    },
+    {
+      id: 13,
+      name: "Laptop MSI Gaming Thin 15 B13UC-2081VN",
+      priceOld: "21.990.000",
+      price: "18.490.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_d_i_5__2.png",
+    },
+    {
+      id: 14,
+      name: "MacBook Pro 14 M4 10CPU 10GPU 16GB 512GB | Chính hãng Apple Việt Nam",
+      priceOld: "39.990.000",
+      price: "37.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/m/a/macbook_4__7.png",
+    },
+    {
+      id: 15,
+      name: "Laptop Lenovo LOQ 15IAX9 83GS000RVN",
+      priceOld: "25.990.000",
+      price: "19.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/g/r/group_509_62_.png",
+    },
+    {
+      id: 16,
+      name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
+      priceOld: "18.990.000",
+      price: "17.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/g/r/group_509_1__1.png",
+    },
+  ],
+
+  phone: [
+    {
+      id: 17,
+      name: "Samsung Galaxy S24 FE 5G 8GB 128GB",
+      priceOld: "16.990.000",
+      price: "13.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/d/i/dien-thoai-samsung-galaxy-s24-fe_3__4.png",
+    },
+    {
+      id: 18,
+      name: "OPPO Reno13 F 8GB 256GB",
+      priceOld: "10.990.000",
+      price: "8.990.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/o/p/oppo-reno13-f-chinh-hang-hinh-2.png",
+    },
+    {
+      id: 19,
+      name: "Samsung Galaxy S25 256GB",
+      priceOld: "22.990.000",
+      price: "18.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/d/i/dien-thoai-samsung-galaxy-s25_1__2.png",
+    },
+    {
+      id: 20,
+      name: "iPhone 13 128GB | Chính hãng VN/A",
+      priceOld: "17.990.000",
+      price: "13.290.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-13_2_.png",
+    },
+    {
+      id: 21,
+      name: "Xiaomi 14T 12GB 512GB",
+      priceOld: "14.990.000",
+      price: "12.990.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/x/i/xiaomi_14t_2_.png",
+    },
+    {
+      id: 22,
+      name: "iPhone 15 Plus 128GB | Chính hãng VN/A",
+      priceOld: "25.990.000",
+      price: "23.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-15-plus_1_.png",
+    },
+    {
+      id: 23,
+      name: "Samsung Galaxy A16 5G 8GB 128GB",
+      priceOld: "9.990.000",
+      price: "8.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-a16-5g_2_.png",
+    },
+  ],
+
+  //   {
+  //     id: 11,
+  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
+  //     priceOld: "14.990.000",
+  //     price: "13.790.000",
+  //     image: "",
+  //   },
+  //   {
+  //     id: 12,
+  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
+  //     priceOld: "14.990.000",
+  //     price: "13.790.000",
+  //     image: "",
+  //   },
+  //   {
+  //     id: 13,
+  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
+  //     priceOld: "14.990.000",
+  //     price: "13.790.000",
+  //     image: "",
+  //   },
+  //   {
+  //     id: 14,
+  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
+  //     priceOld: "14.990.000",
+  //     price: "13.790.000",
+  //     image: "",
+  //   },
+  //   {
+  //     id: 15,
+  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
+  //     priceOld: "14.990.000",
+  //     price: "13.790.000",
+  //     image: "",
+  //   },
+  //   {
+  //     id: 16,
+  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
+  //     priceOld: "14.990.000",
+  //     price: "13.790.000",
+  //     image: "",
+  //   },
+};
+
+const renderData = () => {
+  //  Tìm hiểu về DOMContentLoaded
+  document.addEventListener("DOMContentLoaded", () => {
+    const contentDiv = document.querySelector(".product__list");
+
+    if (contentDiv) {
+      // Kiểm tra tiêu đề trang hoặc URL để xác định trang hiện tại
+      if (document.title === "Trang chủ") {
+        var html = "";
+        data?.outstandingProduct.map((item) => {
+          let content = `
+                <div class="product__item">
+            <img src="${item.image}" alt="">
+            <div class="product__decs">
+                    <h3 class="product__decs-name">${
+                      item.name.length > 40
+                        ? item.name.slice(0, 40) + "..."
+                        : item.name
+                    }</h3>
+                    <h3 class="product__decs-price">
+                        <span class="decs__old-price">
+                        ${item.priceOld} đ
+                        </span>
+                        <span class="decs__new-price">
+                            ${item.price} đ
+                        </span>
+                    </h3>
+            </div>
+            <div class="product__btn">
+                <button class="product__btn-buy">Mua ngay</button>
+                <button onclick="myListAddToCart(''${encodeURIComponent(
+                  JSON.stringify(item)
+                )}')" class="product__btn-add">Thêm vào giỏ</button>
+                
+            </div>
+        </div>  
+              `;
+          html += content;
+        });
+        document.querySelector(".product__list").innerHTML = html;
+      } else if (document.title === "Máy tính xách tay") {
+        let html = "";
+        data?.laptop.map((item) => {
+          let content = `
+                <div class="product__item">
+            <img src="${item.image}" alt="">
+            <div class="product__decs">
+                    <h3 class="product__decs-name">${
+                      item.name.length > 40
+                        ? item.name.slice(0, 40) + "..."
+                        : item.name
+                    }</h3>
+                    <h3 class="product__decs-price">
+                        <span class="decs__old-price">
+                        ${item.priceOld} đ
+                        </span>
+                        <span class="decs__new-price">
+                            ${item.price} đ
+                        </span>
+                    </h3>
+            </div>
+            <div class="product__btn">
+                <button class="product__btn-buy">Mua ngay</button>
+                <button onclick="myListAddToCart(''${encodeURIComponent(
+                  JSON.stringify(item)
+                )}')" class="product__btn-add">Thêm vào giỏ</button>
+            </div>
+        </div>  
+              `;
+          html += content;
+        });
+        document.querySelector(".product__list").innerHTML = html;
+      } else if (document.title === "Điện thoại di động") {
+        let html = "";
+        data?.phone.map((item) => {
+          let content = `
+                <div class="product__item">
+            <img src="${item.image}" alt="">
+            <div class="product__decs">
+                    <h3 class="product__decs-name">${
+                      item.name.length > 40
+                        ? item.name.slice(0, 40) + "..."
+                        : item.name
+                    }</h3>
+                    <h3 class="product__decs-price">
+                        <span class="decs__old-price">
+                        ${item.priceOld} đ
+                        </span>
+                        <span class="decs__new-price">
+                            ${item.price} đ
+                        </span>
+                    </h3>
+            </div>
+            <div class="product__btn">
+                <button class="product__btn-buy">Mua ngay</button>
+                <button onclick="myListAddToCart(''${encodeURIComponent(
+                  JSON.stringify(item)
+                )}')" class="product__btn-add">Thêm vào giỏ</button>
+            </div>
+        </div>  
+              `;
+          html += content;
+        });
+        document.querySelector(".product__list").innerHTML = html;
+      }
+    }
+  });
+};
+
+renderData();
