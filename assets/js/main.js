00c33a0102bdf998aafafc5eb0457d1d01426b90
ThienTran98 <tranhuuthien98@gmail.com>
2025-02-25 17:11:36 +0700

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -194,56 +194,151 @@ const data = {
         "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-a16-5g_2_.png",
     },
   ],
-
-  //   {
-  //     id: 11,
-  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
-  //     priceOld: "14.990.000",
-  //     price: "13.790.000",
-  //     image: "",
-  //   },
-  //   {
-  //     id: 12,
-  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
-  //     priceOld: "14.990.000",
-  //     price: "13.790.000",
-  //     image: "",
-  //   },
-  //   {
-  //     id: 13,
-  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
-  //     priceOld: "14.990.000",
-  //     price: "13.790.000",
-  //     image: "",
-  //   },
-  //   {
-  //     id: 14,
-  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
-  //     priceOld: "14.990.000",
-  //     price: "13.790.000",
-  //     image: "",
-  //   },
-  //   {
-  //     id: 15,
-  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
-  //     priceOld: "14.990.000",
-  //     price: "13.790.000",
-  //     image: "",
-  //   },
-  //   {
-  //     id: 16,
-  //     name: "Laptop Acer Aspire Lite AL16-51P-55N7 NX.KX0SV.001",
-  //     priceOld: "14.990.000",
-  //     price: "13.790.000",
-  //     image: "",
-  //   },
+  tivi: [
+    {
+      id: 25,
+      name: "Smart Tivi NanoCell LG 4K 55 inch 55NANO76SQA",
+      priceOld: "22.990.000",
+      price: "13.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/6/5/65578dfv.png",
+    },
+    {
+      id: 26,
+      name: "Smart Tivi LG QNED 4K 55 inch 2024 (55QNED80TSA)",
+      priceOld: "24.390.000",
+      price: "13.490.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/m/smart_tivi_qned_lg_4k_75_inch_75qned80sra_26_.png",
+    },
+    {
+      id: 27,
+      name: "Smart Tivi LG UHD 4K 43 inch 43UQ7050",
+      priceOld: "10.990.000",
+      price: "7.290.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/4/3/43uq7050.png",
+    },
+    {
+      id: 28,
+      name: "Tivi Xiaomi A Pro 4K 43 inch QLED 2025",
+      priceOld: "8.990.000",
+      price: "7.490.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/m/smart_tivi_qned_lg_4k_75_inch_75qned86sra.png",
+    },
+    {
+      id: 29,
+      name: "Smart Tivi Samsung UHD 4K 43 inch 2024 (43DU7000)1",
+      priceOld: "9.990.000",
+      price: "7.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/m/smart_tivi_oled_lg_4k_48_inch_oled48a3psa_4.png",
+    },
+    {
+      id: 30,
+      name: "Smart Tivi di động LG Stanby Me 27inch Version 2024 27ART10CKPL.ATV",
+      priceOld: "22.990.000",
+      price: "17.990.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/m/smart_tivi_di_ng_lg_stanby_me_27inch_2024.png",
+    },
+    {
+      id: 31,
+      name: "Google Tivi Coocaa 4K 55 inch 55Y73 2024",
+      priceOld: "14.990.000",
+      price: "7.090.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/m/smart_tivi_qned_lg_4k_86_inch_86qned80sra_7_.png",
+    },
+  ],
+  camera: [
+    {
+      id: 32,
+      name: "Máy ảnh kỹ thuật số Sony ZV-E10",
+      priceOld: "16.990.000",
+      price: "14.990.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/m/a/may-anh-sony-zv-e10-0.png",
+    },
+    {
+      id: 33,
+      name: "Máy ảnh kỹ thuật số Sony ZV-11",
+      priceOld: "17.990.000",
+      price: "15.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/m/a/may-anh-sony-zv-1-0.png",
+    },
+    {
+      id: 34,
+      name: "Máy ảnh Fujifilm Instax Camera Mini 12",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/m/a/may-anh-fujifilm-instax-camera-mini-12-ksp.png",
+      priceOld: "2.990.000",
+      price: "2.190.000",
+    },
+    {
+      id: 35,
+      name: "Máy ảnh kỹ thuật số Sony ZV-E10L",
+      priceOld: "18.990.000",
+      price: "16.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/m/a/may-anh-sony-zv-e10l-0.png",
+    },
+    {
+      id: 36,
+      name: "Máy ảnh Fujifilm Instax Mini Evo",
+      priceOld: "5.990.000",
+      price: "5.090.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/m/a/may-anh-fujifilm-instax-mini-evo-0.png",
+    },
+    {
+      id: 37,
+      name: "Máy ảnh kỹ thuật số Sony ZV-1F1",
+      priceOld: "14.990.000",
+      price: "13.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/m/a/may-anh-sony-zv-1f-0.png",
+    },
+    {
+      id: 38,
+      name: "Máy ảnh Instax lấy liền mini 99",
+      priceOld: "4.990.000",
+      price: "4.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/f/u/fujiflim.png",
+    },
+    {
+      id: 39,
+      name: "Máy ảnh Canon Powershot V10",
+      priceOld: "11.990.000",
+      price: "10.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/c/a/canonn_thumbb.png",
+    },
+    {
+      id: 40,
+      name: "Máy ảnh Sony ZV-E10 II / ZV-E10M2 (Kèm Lens Power Zoom 16-50mm)",
+      priceOld: "29.990.000",
+      price: "27.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/m/_/m_y_nh_4_8.png",
+    },
+    {
+      id: 41,
+      name: "Máy ảnh kỹ thuật số Sony ZV-1 II Vlog On The Go_Lite",
+      priceOld: "24.990.000",
+      price: "20.790.000",
+      image:
+        "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/g/r/group_722.png",
+    },
+  ],
 };
-
 const renderData = () => {
   //  Tìm hiểu về DOMContentLoaded
   document.addEventListener("DOMContentLoaded", () => {
     const contentDiv = document.querySelector(".product__list");
-
     if (contentDiv) {
       // Kiểm tra tiêu đề trang hoặc URL để xác định trang hiện tại
       if (document.title === "Trang chủ") {
@@ -269,7 +364,7 @@ const renderData = () => {
             </div>
             <div class="product__btn">
                 <button class="product__btn-buy">Mua ngay</button>
-                <button onclick="myListAddToCart(''${encodeURIComponent(
+                <button onclick="myListAddToCart('${encodeURIComponent(
                   JSON.stringify(item)
                 )}')" class="product__btn-add">Thêm vào giỏ</button>
                 
@@ -334,7 +429,71 @@ const renderData = () => {
             </div>
             <div class="product__btn">
                 <button class="product__btn-buy">Mua ngay</button>
-                <button onclick="myListAddToCart(''${encodeURIComponent(
+                <button onclick="myListAddToCart('${encodeURIComponent(
+                  JSON.stringify(item)
+                )}')" class="product__btn-add">Thêm vào giỏ</button>
+            </div>
+        </div>  
+              `;
+          html += content;
+        });
+        document.querySelector(".product__list").innerHTML = html;
+      } else if (document.title === "Tivi") {
+        let html = "";
+        data?.tivi.map((item) => {
+          let content = `
+                <div class="product__item">
+            <img src="${item.image}" alt="">
+            <div class="product__decs">
+                    <h3 class="product__decs-name">${
+                      item.name.length > 80
+                        ? item.name.slice(0, 80) + "..."
+                        : item.name
+                    }</h3>
+                    <h3 class="product__decs-price">
+                        <span class="decs__old-price">
+                        ${item.priceOld} đ
+                        </span>
+                        <span class="decs__new-price">
+                            ${item.price} đ
+                        </span>
+                    </h3>
+            </div>
+            <div class="product__btn">
+                <button class="product__btn-buy">Mua ngay</button>
+                <button onclick="myListAddToCart('${encodeURIComponent(
+                  JSON.stringify(item)
+                )}')" class="product__btn-add">Thêm vào giỏ</button>
+            </div>
+        </div>  
+              `;
+          html += content;
+        });
+        document.querySelector(".product__list").innerHTML = html;
+      } else if (document.title === "Máy ảnh") {
+        let html = "";
+        data?.camera.map((item) => {
+          let content = `
+                <div class="product__item">
+            <img src="${item.image}" alt="">
+            <div class="product__decs">
+                    <h3 class="product__decs-name">${
+                      item.name.length > 40
+                        ? item.name.slice(0, 40) + "..."
+                        : item.name
+                    }</h3>
+                    <h3 class="product__decs-price">
+                        <span class="decs__old-price">
+                        ${item.priceOld} đ
+                        </span>
+                        <span class="decs__new-price">
+                            ${item.price} đ
+                        </span>
+                    </h3>
+            </div>
+            <div class="product__btn">
+                <button class="product__btn-buy">Mua ngay</button>
+                <button onclick="myListAddToCart('${encodeURIComponent(
                   JSON.stringify(item)
                 )}')" class="product__btn-add">Thêm vào giỏ</button>
             </div>
@@ -349,3 +508,103 @@ const renderData = () => {
 };
 
 renderData();
+
+var myListOrder = JSON.parse(localStorage.getItem("cartItem")) || [];
+
+const myListAddToCart = (items) => {
+  const decodedItem = JSON.parse(decodeURIComponent(items));
+  const index = myListOrder.findIndex((item, index) => {
+    return item.id === decodedItem.id;
+  });
+  if (index === -1) {
+    myListOrder.push(decodedItem);
+    alert(`Sản phẩm ${decodedItem.name} đã được thêm vào giỏ hàng.`);
+
+    console.log("Product added to cart");
+    saveLocalStorage();
+  } else {
+    console.log("Product already in cart");
+    alert(`Sản phẩm ${decodedItem.name} đã có trong vào giỏ hàng.`);
+  }
+};
+
+const saveLocalStorage = () => {
+  var data = JSON.stringify(myListOrder);
+  localStorage.setItem("cartItem", data);
+};
+
+const renderCartOrder = () => {
+  var html = "";
+  if (myListOrder.length > 0) {
+    myListOrder.map((item) => {
+      return (html += `
+      <div class="cart-data">
+            <div class="cart-item">
+                <img src="${item.image}" alt="">
+                <div class="cart-item__decs">
+                    <h3 class="cart-item__decs-name">${
+                      item.name.length > 30
+                        ? item.name.slice(0, 30) + "..."
+                        : item.name
+                    }</h3>
+                    <h3 class="cart-item__decs-price">
+                        <span class="decs__old-price">
+                            ${item.priceOld} đ
+                        </span>
+                        <span class="decs__new-price">
+                            ${item.price} đ
+                        </span>
+                    </h3>
+                </div>
+              <h4 class="cart-item__quantity">
+                    1
+              </h4>
+                <div onclick="handleRemoveItemCart('${
+                  item.id
+                }')" class="cart-item__remove">
+                    <button>Xóa</button>
+                </div> 
+            </div>
+        </div>
+      `);
+    });
+  } else {
+    html += `<div class="cart-no-data">
+            <p>Không có sản phẩm nào trong giỏ hàng của bạn 😢</p>
+            <button><a href="index.html">Tiếp tục mua hàng</a></button>
+        </div>`;
+  }
+
+  document.querySelector(".cart-container").innerHTML = html;
+};
+renderCartOrder();
+
+const handleRemoveItemCart = (item) => {
+  const index = myListOrder.findIndex((i, index) => {
+    return i.id == item;
+  });
+  if (index !== -1) {
+    myListOrder.splice(index, 1);
+    totalPriceOrder();
+    saveLocalStorage();
+    renderCartOrder();
+  } else {
+    return;
+  }
+};
+
+const totalPriceOrder = () => {
+  const totalPrice = myListOrder.reduce((acc, cur) => {
+    return acc + parseInt(cur.price.replace(/\./g, ""), 10);
+  }, 0);
+
+  const formatted = new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  }).format(totalPrice);
+  console.log("formatted: ", formatted);
+
+  document.querySelector(".total-amount-cur").innerHTML = `${formatted}`;
+};
+
+totalPriceOrder();
